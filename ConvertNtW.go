4fc0fdce9a17c36bb0b0c068ee49392386584349
Andrei Melnikov <125220410+amelnikov777@users.noreply.github.com>
2023-03-02 23:24:42 +0200

    ConvertNtW version 3

--- ConvertNtW.go
+++ ConvertNtW.go
@@ -10,6 +10,7 @@ var tensNames = map[int]string{20: "двадцать", 30: "тридцать", 4
 var hundNames = map[int]string{1: "сто", 2: "двести", 3: "триста", 4: "четыреста", 5: "пятьсот", 6: "шестьсот", 7: "семьсот", 8: "восемьсот", 9: "девятьсот"}
 var lowThausNames = map[int]string{0: "тысяч", 1: "одна тысяча", 2: "две тысячи", 3: "три тысячи", 4: "четыре тысячи", 5: "пять тысяч", 6: "шесть тысяч", 7: "семь тысяч", 8: "восемь тысяч", 9: "девять тысяч", 10: "десять тысяч", 11: "одинадцать тысяч", 12: "двенадцать тысяч", 13: "тринадцать тысяч", 14: "четырнадцать тысяч", 15: "пятнадцать тысяч", 16: "шестнадцать тысяч", 17: "семнадцать тысяч", 18: "восемьнадцать тысяч", 19: "девятнадцать тысяч"}
 var lowMillNames = map[int]string{0: "миллион", 1: "один миллион", 2: "два миллиона", 3: "три миллиона", 4: "четыре миллиона", 5: "пять миллионов", 6: "шесть миллионов", 7: "семь миллионов", 8: "восемь миллионов", 9: "девять миллионов", 10: "десять миллионов", 11: "одинадцать миллионов", 12: "двенадцать миллионов", 13: "тринадцать миллионов", 14: "четырнадцать миллионов", 15: "пятнадцать миллионов", 16: "шестнадцать миллионов", 17: "семнадцать миллионов", 18: "восемьнадцать миллионов", 19: "девятнадцать миллионов"}
+var lowMlrdNames = map[int]string{0: "миллиард", 1: "один миллиард", 2: "два миллиарда", 3: "три миллиарда", 4: "четыре миллиарда", 5: "пять миллиардов", 6: "шесть миллиардов", 7: "семь миллиардов", 8: "восемь миллиардов", 9: "девять миллиардов", 10: "десять миллиардов", 11: "одинадцать миллиардов", 12: "двенадцать миллиардов", 13: "тринадцать миллиардов", 14: "четырнадцать миллиардов", 15: "пятнадцать миллиардов", 16: "шестнадцать миллиардов", 17: "семнадцать миллиардов", 18: "восемьнадцать миллиардов", 19: "девятнадцать миллиардов"}
 
 func convert100(num int) string {
 	if num < 20 {
@@ -59,6 +60,23 @@ func convert100M(num int) string {
 	return "convert100M"
 }
 
+func convert100Md(num int) string {
+	if num == 0 {
+		return ""
+	}
+	if num < 20 {
+		return lowMlrdNames[num]
+	}
+	if num < 100 {
+		if num%10 == 0 {
+			return tensNames[num] + lowMlrdNames[0]
+		} else {
+			return tensNames[num/10*10] + " " + lowMlrdNames[num%10]
+		}
+	}
+	return "convert100Md"
+}
+
 func convert999(num int) string {
 	if num < 100 {
 		return convert100(num)
@@ -83,29 +101,38 @@ func convert999M(num int) string {
 	}
 }
 
+func convert999Md(num int) string {
+	if num < 100 {
+		return convert100Md(num)
+	} else {
+		return hundNames[num/100] + " " + convert100Md(num%100)
+	}
+}
+
 func convertNumToWord(num int) string {
-	if num == 0 {
+	switch {
+	case num == 0:
 		return "ноль"
-	}
-	if num < 1000 {
+	case num < 1000:
 		return convert999(num)
-	}
-	if num < 1000000 {
+	case num < 1000000:
 		return convert999K(num/1000) + " " + convert999(num%1000)
-	}
-	if num < 1000000000 {
+	case num < 1000000000:
 		return convert999M(num/1000000) + " " + convert999K((num/1000)%1000) + " " + convert999(num%1000)
+	case num < 1000000000000:
+		return convert999Md(num/1000000000) + " " + convert999M((num/1000000)%1000) + " " + convert999K((num/1000)%1000) + " " + convert999(num%1000)
 	}
-	return "Число больше 999 миллионов"
+	return "Число больше 999 миллиардов"
 }
 
 func main() {
 	var i int
 	fmt.Println("Введите число для конвертации")
 	fmt.Scanln(&i)
-	if i >= 0 {
+	switch {
+	case i >= 0:
 		fmt.Println(i, " ", strings.Replace(convertNumToWord(i), "  ", " ", -1))
-	} else {
+	case i < 0:
 		fmt.Println(i, " минус", strings.Replace(convertNumToWord(i*-1), "  ", " ", -1))
 	}
 
